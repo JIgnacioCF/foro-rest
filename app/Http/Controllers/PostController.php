e50653533e770106fcb75c146968a59cbda24b9d
JIgnacioCF <111895507+JIgnacioCF@users.noreply.github.com>
2022-10-18 11:35:59 -0500

    sesion 181022

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+
+class PostController extends Controller
+{
+    public function index(){
+        //return Post::paginate();
+        return Post::paginate();
+    }
+
+    public function show($id){
+       
+        $post= Post::find($id);
+        if(!$post){
+            return response()->json(["message"=>"failed"], 404);
+        }
+        return $post;
+    }
+
+    public function store(Request $request){
+        $post = new Post;
+        $r = $post->fill($request->all())->save();
+        if(!$r){
+            return response()->json(["message"=>"failed"],404);
+        }
+        return $post;
+    }
+
+    public function update(Request $request, $id){
+        $post = Post::find($id);
+        if(!$post){
+            return response()->json(["message"=>"failed"], 404);
+        }
+        $r = $post->fill($request->all())->save();
+        if(!$r){
+            return response()->json(["message"=>"failed"],404);
+        }
+        return $post;
+    }
+
+    public function destroy($id){
+        $post = Post::find($id);
+        if(!$post){
+            return response()->json(["message"=>"failed"], 404);
+    }
+    $post->delete();
+    return response()->json(["message"=>"succes"]);
+}
+}
---
+++ app/Http/Controllers/TopicController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Topic;
+use Illuminate\Http\Request;
+
+
+class TopicController extends Controller
+{
+    public function index(){
+        //return Topic::paginate();
+        return Topic::paginate();
+    }
+
+    public function show($id){
+       
+        $topic= Topic::find($id);
+        if(!$topic){
+            return response()->json(["message"=>"failed"], 404);
+        }
+        return $topic;
+    }
+
+    public function store(Request $request){
+        $topic = new Topic;
+        $r = $topic->fill($request->all())->save();
+        if(!$r){
+            return response()->json(["message"=>"failed"],404);
+        }
+        return $topic;
+    }
+
+    public function update(Request $request, $id){
+        $topic = Topic::find($id);
+        if(!$topic){
+            return response()->json(["message"=>"failed"], 404);
+        }
+        $r = $topic->fill($request->all())->save();
+        if(!$r){
+            return response()->json(["message"=>"failed"],404);
+        }
+        return $topic;
+    }
+
+    public function destroy($id){
+        $topic = Topic::find($id);
+        if(!$topic){
+            return response()->json(["message"=>"failed"], 404);
+    }
+    $topic->delete();
+    return response()->json(["message"=>"succes"]);
+}
+}
