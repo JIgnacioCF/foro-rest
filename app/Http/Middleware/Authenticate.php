273b13efe53e79ed6ec4ec9a0620eeefce103b7c
JIgnacioCF <111895507+JIgnacioCF@users.noreply.github.com>
2022-10-20 14:29:53 -0500

    Autenticacion

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -36,7 +36,7 @@ class Authenticate
     public function handle($request, Closure $next, $guard = null)
     {
         if ($this->auth->guard($guard)->guest()) {
-            return response('Unauthorized.', 401);
+            return response()->json(['status' =>'failed'], 404);
         }
 
         return $next($request);
--- routes/web.php
+++ routes/web.php
@@ -12,20 +12,22 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+$router->get('/login', 'AuthController@login');
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-function resource($router, $url, $model){
-$router->get($url, $model.'Controller@index');
-$router->get($url.'/{id}', $model.'Controller@show');
-$router->post($url, $model.'Controller@store');
-$router->put($url.'/{id}', $model.'Controller@update');
-$router->delete($url.'/{id}', $model.'Controller@destroy');
+function resource($router, $url, $model)
+{
+    $router->get($url, $model . 'Controller@index');
+    $router->get($url . '/{id}', $model . 'Controller@show');
+    $router->post($url, $model . 'Controller@store');
+    $router->put($url . '/{id}', $model . 'Controller@update');
+    $router->delete($url . '/{id}', $model . 'Controller@destroy');
 }
-
-resource($router, '/topics', 'Topic');
-resource($router, '/users', 'User');
-resource($router, '/posts', 'Post');
-
+$router->group(['middleware' => 'auth'], function () use ($router) {
+    resource($router, '/topics', 'Topic');
+    resource($router, '/users', 'User');
+    resource($router, '/posts', 'Post');
+});
